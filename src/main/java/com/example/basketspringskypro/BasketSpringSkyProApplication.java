5a036007a3fa19ceb7076096ec7d86db3f7e982d
Kulachenkov <nik.k2710@gmail.com>
2022-02-17 15:06:07 +0300

    Initial commit

---
+++ src/main/java/com/example/basketspringskypro/BasketSpringSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.example.basketspringskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketSpringSkyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketSpringSkyProApplication.class, args);
+    }
+
+}
